afd8a68cbea846bea752c916c45d98ed7aa94d42
Diego Candido <diegoscandido19@gmail.com>
2025-01-14 17:43:45 -0300

    iniciando projeto de aula react alura

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,31 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+  return (
+    <footer>
+      <section>
+        <ul>
+          <li>
+            <a href="facebook.com" target="blank">
+              <img src="./public/imagens/fb.png" />
+            </a>
+          </li>
+          <li>
+            <a href="twitter"></a>
+          </li>
+          <li>
+            <a href="Instagram"></a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <img>logo</img>
+      </section>
+      <section>
+        <p>Desenvolvido por Diego Candido</p>
+      </section>
+    </footer>
+  );
+};
+
+export default Rodape;
